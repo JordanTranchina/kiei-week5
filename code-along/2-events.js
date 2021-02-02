f41dcc4ce8d5b42ddb7e0d9ef116e61a83f73acb
Jordan Tranchina <Jordan12522@gmail.com>
2021-02-02 14:35:45 -0600

    prepping for 2 events code along

--- code-along/2-events.js
+++ code-along/2-events.js
@@ -1,3 +1,17 @@
 // To get started:
+
+
+
 // 1. add an event listener to the window object, listening for the "DOMContentLoaded" event
+
+
+
+
+
+
 // 2. use an anonymous function as the listener
+
+
+
+
+
